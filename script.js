47b6320836576d6919d4673b9f203c9c53617b9a
saikumarchittaluri <127985962+saikumarchittaluri@users.noreply.github.com>
2023-06-15 17:58:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-function letterCombinations(input_digit) {
+function letterCombinations(input) {
   //Complete the function
+	let table{
+		"1" : "1"
+		"2" : "abc",
+		"3" : "def",
+		"4" : "ghi",
+		"5" : "jkl",
+		"6" : "mno",
+		"7" : "pqrs",
+		"8" : "tuv",
+		"9" : "wxyz",
+		"0" : "0"
+	}
+let Solution = [];
+function solve (currentIndex, calculatedString){
+	if(input.length <= currentIndex){
+		Solution.push(calculatedString);
+		return;
+	}
+	let currentChar=input[currentIndex];
+	for(let i=0;i<table[currentChar].length;i++){
+		solve(currentIndex+1, calculatedString + table[currentChar])
+	}
+}
+solve(0,"");
+console.log(Solution);
 }
 
 module.exports = letterCombinations;
